a7c01ad161d249a6c76b5595e969c953b83d971b
Davide Valsecchi <davide.vals94@gmail.com>
2012-07-16 13:32:38 +0200

    aggiunto salvataggio dati all'uscita da DetaIntegrityReportDetailWIndow

--- QuickAgenda/src/it/valsecchi/quickagenda/windows/OptionsWindow.java
+++ QuickAgenda/src/it/valsecchi/quickagenda/windows/OptionsWindow.java
@@ -1,6 +1,10 @@
 package it.valsecchi.quickagenda.windows;
 
 import it.valsecchi.quickagenda.data.DataManager;
+import it.valsecchi.quickagenda.data.report.DataIntegrityReportResult;
+import it.valsecchi.quickagenda.data.report.ReportsManager;
+import it.valsecchi.quickagenda.windows.detail.DataIntegrityReportDetailWindow;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -22,6 +26,7 @@ public class OptionsWindow extends JFrame {
 	private JButton btnSalvaFileBackup;
 	private DataManager data;
 	private Timer timer1;
+	private Timer timer2;
 	private ShowProgressWindow progress;
 	private JButton btnControlloIntegritaDati;
 
@@ -41,27 +46,43 @@ public class OptionsWindow extends JFrame {
 		btnSalvaFileBackup.setIcon(new ImageIcon(OptionsWindow.class
 				.getResource("/ico_small/filesaveas.png")));
 		btnControlloIntegritaDati = new JButton("Controllo Integrit\u00E0 Dati");
-		btnControlloIntegritaDati.addActionListener(new BtnControlloIntegritaDatiActionListener());
-		btnControlloIntegritaDati.setIcon(new ImageIcon(OptionsWindow.class.getResource("/ico_small/preferences_system_session.png")));
+		btnControlloIntegritaDati
+				.addActionListener(new BtnControlloIntegritaDatiActionListener());
+		btnControlloIntegritaDati.setIcon(new ImageIcon(OptionsWindow.class
+				.getResource("/ico_small/preferences_system_session.png")));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnSalvaFileBackup)
-						.addComponent(btnControlloIntegritaDati, GroupLayout.PREFERRED_SIZE, 239, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(303, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(24)
-					.addComponent(btnSalvaFileBackup, GroupLayout.PREFERRED_SIZE, 83, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(btnControlloIntegritaDati, GroupLayout.PREFERRED_SIZE, 83, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(95, Short.MAX_VALUE))
-		);
+		gl_contentPane
+				.setHorizontalGroup(gl_contentPane
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addComponent(
+																btnSalvaFileBackup)
+														.addComponent(
+																btnControlloIntegritaDati,
+																GroupLayout.PREFERRED_SIZE,
+																239,
+																GroupLayout.PREFERRED_SIZE))
+										.addContainerGap(303, Short.MAX_VALUE)));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(
+				Alignment.LEADING).addGroup(
+				gl_contentPane
+						.createSequentialGroup()
+						.addGap(24)
+						.addComponent(btnSalvaFileBackup,
+								GroupLayout.PREFERRED_SIZE, 83,
+								GroupLayout.PREFERRED_SIZE)
+						.addGap(18)
+						.addComponent(btnControlloIntegritaDati,
+								GroupLayout.PREFERRED_SIZE, 83,
+								GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(95, Short.MAX_VALUE)));
 		contentPane.setLayout(gl_contentPane);
 	}
 
@@ -99,10 +120,34 @@ public class OptionsWindow extends JFrame {
 			timer1.start();
 		}
 	}
-	private class BtnControlloIntegritaDatiActionListener implements ActionListener {
+
+	private class BtnControlloIntegritaDatiActionListener implements
+			ActionListener {
 		public void actionPerformed(ActionEvent arg) {
-			//si crea il report
-			
+			// si crea il report
+			progress = new ShowProgressWindow(
+					"Creazione report integrità dati...",
+					"Creazione report integrità dati",
+					new ImageIcon(
+							OptionsWindow.class
+									.getResource("/ico_small/preferences_system_session.png")));
+			progress.setVisible(true);
+			final DataIntegrityReportResult report = ReportsManager
+					.performDataIntegrityReport(data);
+			// timer
+			timer2 = new Timer(1500, new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					// si ferma il timer
+					timer2.stop();
+					// si chiude progress.
+					progress.dispose();
+					//si apre la finestra dettagli
+					DataIntegrityReportDetailWindow detail = new DataIntegrityReportDetailWindow(report,data);
+					detail.setVisible(true);
+				}
+			});
+			timer2.start();
 		}
 	}
 }
--- QuickAgenda/src/it/valsecchi/quickagenda/windows/detail/DataIntegrityReportDetailWindow.java
+++ QuickAgenda/src/it/valsecchi/quickagenda/windows/detail/DataIntegrityReportDetailWindow.java
@@ -1,7 +1,11 @@
 package it.valsecchi.quickagenda.windows.detail;
 
+import it.valsecchi.quickagenda.data.DataManager;
+import it.valsecchi.quickagenda.data.exception.CryptographyException;
 import it.valsecchi.quickagenda.data.report.DataIntegrityReportResult;
-
+import it.valsecchi.quickagenda.settings.SettingsManager;
+import it.valsecchi.quickagenda.windows.ShowProgressWindow;
+import static it.valsecchi.quickagenda.data.Utility.Log;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -11,16 +15,26 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Toolkit;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
+import javax.swing.Timer;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 public class DataIntegrityReportDetailWindow extends JFrame {
 
 	private static final long serialVersionUID = 1895882931117616679L;
 	private JPanel contentPane;
 	private DataIntegrityReportResult report;
+	private DataManager data;
+	private Timer timer1;
 	private JLabel lblNTotaleDi;
 	private JTextField txtNTot;
 	private JLabel lblNTotaleDi_1;
@@ -32,16 +46,23 @@ public class DataIntegrityReportDetailWindow extends JFrame {
 	private JLabel lblNTotaleDi_4;
 	private JTextField txtSessioni;
 
-	public DataIntegrityReportDetailWindow(DataIntegrityReportResult _report) {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(DataIntegrityReportDetailWindow.class.getResource("/ico_small/preferences_system_session.png")));
+	public DataIntegrityReportDetailWindow(DataIntegrityReportResult _report,
+			DataManager _data) {
+		addWindowListener(new ThisWindowListener());
+		setIconImage(Toolkit
+				.getDefaultToolkit()
+				.getImage(
+						DataIntegrityReportDetailWindow.class
+								.getResource("/ico_small/preferences_system_session.png")));
 		setTitle("Dettagli Rapporto Integrit\u00E0 Dati");
+		data = _data;
 		report = _report;
 		initComponent();
 		initData();
 	}
-	
-	private void initComponent(){
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	private void initComponent() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 635, 417);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -67,70 +88,210 @@ public class DataIntegrityReportDetailWindow extends JFrame {
 		txtSessioni.setEditable(false);
 		txtSessioni.setColumns(10);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNTotaleDi)
-							.addGap(18)
-							.addComponent(txtNTot, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(40)
-							.addComponent(lblNTotaleDi_1, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(txtErrori, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNTotaleDi_2, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(txtClienti, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNTotaleDi_3, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(txtLavori, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNTotaleDi_4, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(txtSessioni, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(52, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(21)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNTotaleDi)
-						.addComponent(txtNTot, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNTotaleDi_1)
-						.addComponent(txtErrori, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblNTotaleDi_2))
-						.addComponent(txtClienti, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblNTotaleDi_3))
-						.addComponent(txtLavori, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblNTotaleDi_4))
-						.addComponent(txtSessioni, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(199, Short.MAX_VALUE))
-		);
+		gl_contentPane
+				.setHorizontalGroup(gl_contentPane
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addComponent(
+																				lblNTotaleDi)
+																		.addGap(18)
+																		.addComponent(
+																				txtNTot,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(40)
+																		.addComponent(
+																				lblNTotaleDi_1,
+																				GroupLayout.PREFERRED_SIZE,
+																				124,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				txtErrori,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE))
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addComponent(
+																				lblNTotaleDi_2,
+																				GroupLayout.PREFERRED_SIZE,
+																				124,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(18)
+																		.addComponent(
+																				txtClienti,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE))
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addComponent(
+																				lblNTotaleDi_3,
+																				GroupLayout.PREFERRED_SIZE,
+																				124,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(18)
+																		.addComponent(
+																				txtLavori,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE))
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addComponent(
+																				lblNTotaleDi_4,
+																				GroupLayout.PREFERRED_SIZE,
+																				124,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(18)
+																		.addComponent(
+																				txtSessioni,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE)))
+										.addContainerGap(52, Short.MAX_VALUE)));
+		gl_contentPane
+				.setVerticalGroup(gl_contentPane
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addGap(21)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(
+																lblNTotaleDi)
+														.addComponent(
+																txtNTot,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																lblNTotaleDi_1)
+														.addComponent(
+																txtErrori,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE))
+										.addGap(18)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addGap(3)
+																		.addComponent(
+																				lblNTotaleDi_2))
+														.addComponent(
+																txtClienti,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE))
+										.addGap(18)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addGap(3)
+																		.addComponent(
+																				lblNTotaleDi_3))
+														.addComponent(
+																txtLavori,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE))
+										.addGap(18)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addGap(3)
+																		.addComponent(
+																				lblNTotaleDi_4))
+														.addComponent(
+																txtSessioni,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE))
+										.addContainerGap(199, Short.MAX_VALUE)));
 		contentPane.setLayout(gl_contentPane);
 	}
-	
-	public void initData(){
+
+	public void initData() {
 		txtNTot.setText(Integer.toString(report.getN_tot_elements()));
 		txtErrori.setText(Integer.toString(report.getN_errors()));
 		txtClienti.setText(Integer.toString(report.getN_costumers()));
 		txtLavori.setText(Integer.toString(report.getN_works()));
 		txtSessioni.setText(Integer.toString(report.getN_sessions()));
+	}
+
+	private class ThisWindowListener extends WindowAdapter {
+		@Override
+		public void windowClosing(WindowEvent arg0) {
+			final ShowProgressWindow progress = new ShowProgressWindow(
+					"Salvataggio dati in corso...",
+					"Salvataggio dati",
+					new ImageIcon(getClass().getResource("/ico_small/save.png")));
+			progress.setVisible(true);
+			try {
+				// SALVATAGGIO DATI
+				Log.info("salvataggio dati e preferenze");
+				data.saveData();
+				// si salvano le preferenze
+				SettingsManager.writeSettings();
+				// si chiude la finestra
+				progress.setIcon(new ImageIcon(getClass().getResource(
+						"/ico_small/check.png")));
+				progress.setMessage("Salvataggio completato!");
+				// timer
+				timer1 = new Timer(1500, new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						// si ferma il timer
+						timer1.stop();
+						// si chiude progress.
+						progress.dispose();
+					}
+				});
+				timer1.start();
+			} catch (CryptographyException e) {
+				// in caso si errore non si chiude la finestra
+				Log.error("errore di criptografia");
+				progress.setIcon(new ImageIcon(getClass().getResource(
+						"/ico_small/warning.png")));
+				progress.setMessage("Errore di criptografia! Dati non salvati. Riprovare.");
+			} catch (IOException e) {
+				Log.error("errore IO");
+				progress.setIcon(new ImageIcon(getClass().getResource(
+						"/ico_small/warning.png")));
+				progress.setMessage("Errore file! Impossibile scrivere dati! Riprovare.");
+			}
+		}
 	}
 }
